2da014b109c19b319bdf87edf73514a22ccb10fa
Nicholas Petrone <nickpetrone92@gmail.com>
2020-02-16 07:13:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -34,9 +34,21 @@ class CashRegister
     end
   end
 
-
-  def void_last_transaction()
-    @total - @last_transaction_amount
-  end
+def add_item(title, price, quantity=1)
+    @total += price * quantity 
+    quantity.times do 
+      @items << title 
+    end 
+    @last_item = price * quantity 
+  end 
+  
+  def items 
+    @items 
+  end 
+  
+  def void_last_transaction 
+     @items.delete_at(-1)
+    self.total = self.total - @last_item
+  end 
 
 end
